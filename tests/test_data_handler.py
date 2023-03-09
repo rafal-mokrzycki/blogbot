fe97312532be45ed27aee68d5467a1f427068e2f
rafal-mokrzycki <55636265+rafal-mokrzycki@users.noreply.github.com>
2023-03-09 18:41:13 +0100

    Update test_data_handler.py

--- tests/test_data_handler.py
+++ tests/test_data_handler.py
@@ -4,3 +4,33 @@ add --pdb to debug in command line
 use VScode test tools recommended (the beaker)
 to debug VScode: pytest --collect-only
 """
+import datetime
+import os
+import pathlib
+import random
+import tempfile
+from time import time
+
+import numpy as np
+import pandas
+import pytest
+
+from scripts.data_handler import Transformer
+
+
+def test_transform():
+    string = Transformer.transform("Today the weather is beautiful.")[0]["generated_text"]
+    print(string)
+    assert string.startswith("Today the weather is beautiful.")
+
+
+def test_transform_wrong_input():
+    pass
+
+
+def test_transform_wrong_max_length():
+    pass
+
+
+def test_transform_wrong_n_sentences():
+    pass
