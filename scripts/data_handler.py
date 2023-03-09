b0822d2f6bd6458b29d878ce7d95c56f90c186ec
Rafał Mokrzycki <rafalmokrzycki1@gmail.com>
2023-03-09 19:42:35 +0100

    Trasformation engine (#3)
    * Add Transformer class
    
    * Update .gitignore
    
    * Create __init__.py
    
    * Create __init__.py
    
    * Create test_data_handler.py
    
    * Create test_gcs_connector.py
    
    * Update data_handler.py
    
    * Update test_data_handler.py
    
    * Update test_data_handler.py

---
+++ scripts/data_handler.py
@@ -0,0 +1,33 @@
+from transformers import pipeline
+
+
+class Collector:
+    pass
+
+
+class Transformer:
+    def transform(input: str, temperature=0.6, max_length: int = 256, n_sentences: int = 1):
+        """Transforms an input sentence into a longer paragraph.
+
+        Args:
+            input (str): _description_
+            max_length (int, optional): _description_. Defaults to 256.
+            n_sentences (int, optional): _description_. Defaults to 1.
+
+        Returns:
+            list: _description_
+        """
+        generator = pipeline("text-generation", model="EleutherAI/gpt-neo-125M")
+        return generator(
+            input,
+            do_sample=True,
+            top_k=50,
+            temperature=temperature,
+            max_length=max_length,
+            num_return_sequences=n_sentences,
+            pad_token_id=generator.tokenizer.eos_token_id,
+        )
+
+
+class Distributor:
+    pass
---
+++ tests/test_gcs_connector.py
@@ -0,0 +1,6 @@
+"""
+to run: python -m pytest -vv tests\test_data_handler.py -s
+add --pdb to debug in command line
+use VScode test tools recommended (the beaker)
+to debug VScode: pytest --collect-only
+"""
