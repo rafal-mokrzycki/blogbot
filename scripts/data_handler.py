c38934ce21fb91fbeb51058523a7116b0e98906c
rafal-mokrzycki <55636265+rafal-mokrzycki@users.noreply.github.com>
2023-03-09 18:41:09 +0100

    Update data_handler.py

--- scripts/data_handler.py
+++ scripts/data_handler.py
@@ -6,7 +6,7 @@ class Collector:
 
 
 class Transformer:
-    def transform(input: str, max_length: int = 256, n_sentences: int = 1):
+    def transform(input: str, temperature=0.6, max_length: int = 256, n_sentences: int = 1):
         """Transforms an input sentence into a longer paragraph.
 
         Args:
@@ -17,14 +17,15 @@ class Transformer:
         Returns:
             list: _description_
         """
-        gpt2_generator = pipeline("text-generation", model="EleutherAI/gpt-neo-125M")
-        return gpt2_generator(
+        generator = pipeline("text-generation", model="EleutherAI/gpt-neo-125M")
+        return generator(
             input,
             do_sample=True,
             top_k=50,
-            temperature=0.6,
+            temperature=temperature,
             max_length=max_length,
             num_return_sequences=n_sentences,
+            pad_token_id=generator.tokenizer.eos_token_id,
         )
 
 
