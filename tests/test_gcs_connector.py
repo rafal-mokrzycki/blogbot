9004fb770310dada1c57c47d432cba0253979840
rafal-mokrzycki <55636265+rafal-mokrzycki@users.noreply.github.com>
2023-03-11 16:06:13 +0100

    Add test_create_unique_bucket_name and test_delete_blob

--- tests/test_gcs_connector.py
+++ tests/test_gcs_connector.py
@@ -15,6 +15,10 @@ def gcs():
     return GCS_Connector()
 
 
+def test_create_unique_bucket_name(gcs):
+    pass
+
+
 def test_create_unique_blob_name(gcs):
     unique_names = []
     for _ in range(3):
@@ -40,3 +44,7 @@ def test_list_blobs(gcs):
     gcs.write_file_to_bucket("input text", bucket_name, file_name)
     assert len(gcs.list_blobs("tmp-bucket")) == 1
     gcs.delete_blob(bucket_name, file_name)
+
+
+def test_delete_blob(gcs):
+    pass
