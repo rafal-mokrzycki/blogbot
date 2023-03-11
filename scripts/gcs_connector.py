34fcd69e9e3c80a66ca47b3906f68804aa7be0fc
rafal-mokrzycki <55636265+rafal-mokrzycki@users.noreply.github.com>
2023-03-11 16:02:36 +0100

    Update delete_blob method

--- scripts/gcs_connector.py
+++ scripts/gcs_connector.py
@@ -128,27 +128,12 @@ class GCS_Connector:
 
     def delete_blob(self, bucket_name, blob_name):
         """Deletes a blob from the bucket"""
-        # TODO: adjust to project needs.
-        # bucket_name = "your-bucket-name"
-        # blob_name = "your-object-name"
-
-        storage_client = storage.Client()
-
-        bucket = storage_client.bucket(bucket_name)
+        bucket = self.client.bucket(bucket_name)
         blob = bucket.blob(blob_name)
-        generation_match_precondition = None
-
-        # Optional: set a generation-match precondition to avoid
-        # potential race conditions and data corruptions. The request
-        # to delete is aborted if the object's generation number
-        # does not match your precondition.
-        blob.reload()
-        # Fetch blob metadata to use in generation_match_precondition.
-        generation_match_precondition = blob.generation
-
-        blob.delete(if_generation_match=generation_match_precondition)
-
-        print(f"Blob {blob_name} deleted.")
+        self.log.warning(
+            f"Object {blob_name} deleted in bucket: {bucket_name}"
+        )
+        blob.delete()
 
     def list_blobs(self, bucket_name):
         """Lists all the blobs in the bucket."""
