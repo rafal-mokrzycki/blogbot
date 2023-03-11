c2c63b8e0fe046fc67c00facf405d99fba112e3b
rafal-mokrzycki <55636265+rafal-mokrzycki@users.noreply.github.com>
2023-03-11 15:58:01 +0100

    Update test_gcs_connector.py

--- tests/test_gcs_connector.py
+++ tests/test_gcs_connector.py
@@ -10,13 +10,22 @@ from scripts.gcs_connector import GCS_Connector
 
 
 @pytest.fixture(scope="module")
-def gcs_connection():
+def gcs():
     """Creates and returns a Google Cloud Storage connection"""
     return GCS_Connector()
 
 
-def test_create_unique_blob_name(gcs_connection):
+def test_create_unique_blob_name(gcs):
     unique_names = []
     for _ in range(3):
-        unique_names.append(gcs_connection.create_unique_blob_name())
+        unique_names.append(gcs.create_unique_blob_name())
     assert len(unique_names) == len(set(unique_names))
+
+
+def test_get_project_id(gcs):
+    assert gcs.get_project_id() == "thinking-glass-380312"
+
+
+def test_write_file_to_bucket(gcs):
+    gcs.write_file_to_bucket("input text", "tmp-bucket", "tmp-file.txt")
+    assert "tmp-file.txt" in gcs.list_blobs("tmp-bucket")
