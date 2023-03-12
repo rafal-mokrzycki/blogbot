5201176ba9532af8a9e557c9f74d1eb9f2f6ddf1
rafal-mokrzycki <55636265+rafal-mokrzycki@users.noreply.github.com>
2023-03-12 15:08:08 +0100

    Refactorization

--- scripts/gcs_connector.py
+++ scripts/gcs_connector.py
@@ -44,29 +44,6 @@ class GCS_Connector:
         """Return project name from client google.storage.client object."""
         return self.client.project
 
-    def write_blob_to_bucket(
-        self, input_text: str, bucket_name: str = None, blob_name: str = None
-    ) -> None:
-        """Writes a string into a .txt file in a given bucket.
-
-        Args:
-            input_text (str): a blog post (output of
-            Transformer.transform method)
-            bucket_name (str, optional): user's bucket name.
-            Defaults to None.
-            blob_name (str, optional): .txt file name. Defaults to None.
-        """
-        if bucket_name is None:
-            bucket_name = self.create_unique_bucket_name()
-            # TODO: check if a bucket for that client exists,
-            # if not, create it
-        if blob_name is None:
-            blob_name = self.create_unique_blob_name()
-        storage_client = storage.Client()
-        bucket = storage_client.bucket(bucket_name)
-        blob = bucket.blob(blob_name)
-        blob.upload_from_string(input_text)
-
     def create_unique_bucket_name(self) -> str:
         """Creates an unique bucket name
         based on the client account email.
@@ -95,36 +72,81 @@ class GCS_Connector:
                     (see: https://cloud.google.com/storage/docs/buckets#naming"
             )
 
+    def create_new_bucket(
+        self,
+        bucket_name: str,
+        storage_class: str = "STANDARD",
+        location: str = "us",
+    ):
+        """Creates a new bucket.
+        Used when a new client registers to the app.
+
+        Args:
+            bucket_name (str): new bucket name
+            storage_class (str, optional): storage class.
+            Defaults to "STANDARD".
+            location (str, optional): bucket location. Defaults to "us".
+
+        Returns:
+            _type_: new bucket
+        """
+        bucket = self.client.bucket(bucket_name)
+        bucket.storage_class = storage_class
+        new_bucket = self.client.create_bucket(bucket, location=location)
+        return new_bucket
+
+    def list_buckets(self):
+        """Lists all buckets."""
+        buckets = self.client.list_buckets()
+        return [bucket.name for bucket in buckets]
+
+    def delete_bucket(self, bucket_name: str) -> None:
+        """Deletes a bucket.
+        Used when a client closes their account.
+
+        Args:
+            bucket_name (str): bucket to be deleted.
+        """
+        bucket = self.client.get_bucket(bucket_name)
+        bucket.delete()
+
     def create_unique_blob_name(self) -> str:
         """Creates an unique blob name
         based on the timestamp.
 
         Returns:
-            str: blob name in format '20230311124542597264.txt'
+            str: blob name in format 'YYYYMMDDhhmmssffffff.txt'
         """
         time.sleep(0.1)
         return datetime.now().strftime("%Y%m%d%H%M%S%f") + ".txt"
 
-    def create_bucket_class_location(self, bucket_name):
-        """
-        Create a new bucket in the US region with the coldline storage
-        class
-        """
-        # TODO: adjust to project needs.
-        # bucket_name = "your-new-bucket-name"
+    def write_blob_to_bucket(
+        self, input_text: str, bucket_name: str = None, blob_name: str = None
+    ) -> None:
+        """Writes a string into a .txt file in a given bucket.
 
+        Args:
+            input_text (str): a blog post (output of
+            Transformer.transform method)
+            bucket_name (str, optional): user's bucket name.
+            Defaults to None.
+            blob_name (str, optional): .txt file name. Defaults to None.
+        """
+        if bucket_name is None:
+            bucket_name = self.create_unique_bucket_name()
+            # TODO: check if a bucket for that client exists,
+            # if not, create it
+        if blob_name is None:
+            blob_name = self.create_unique_blob_name()
         storage_client = storage.Client()
-
         bucket = storage_client.bucket(bucket_name)
-        bucket.storage_class = "COLDLINE"
-        new_bucket = storage_client.create_bucket(bucket, location="us")
+        blob = bucket.blob(blob_name)
+        blob.upload_from_string(input_text)
 
-        print(
-            "Created bucket {} in {} with storage class {}".format(
-                new_bucket.name, new_bucket.location, new_bucket.storage_class
-            )
-        )
-        return new_bucket
+    def list_blobs(self, bucket_name):
+        """Lists all the blobs in the bucket."""
+        blobs = self.client.list_blobs(bucket_name)
+        return [blob.name for blob in blobs]
 
     def delete_blob(self, bucket_name, blob_name):
         """Deletes a blob from the bucket"""
@@ -134,13 +156,3 @@ class GCS_Connector:
             f"Object {blob_name} deleted in bucket: {bucket_name}"
         )
         blob.delete()
-
-    def list_blobs(self, bucket_name):
-        """Lists all the blobs in the bucket."""
-        blobs = self.client.list_blobs(bucket_name)
-        return [blob.name for blob in blobs]
-
-    def list_buckets(self):
-        """Lists all buckets."""
-        buckets = self.client.list_buckets()
-        return [bucket.name for bucket in buckets]
